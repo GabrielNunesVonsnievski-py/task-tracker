306755745d32621abd822bdc5d0b8c062ce59a88
GabrielNunesVonsnievski-py <gabriel_nunes2017@outlook.com>
2025-06-02 15:02:16 -0300

    Função de disabled de botao play and pause

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -5,21 +5,7 @@
                 <input type="text" class="input" placeholder="Qual tarefa você deseja inicar?">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <CronometroFormulario :tempoEmSegundos="tempoEmSegundos"/>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <TemporizadorTarefa />
             </div>
         </div>
     </div>
@@ -28,30 +14,12 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue';
-import CronometroFormulario from './CronometroFormulario.vue'
+import TemporizadorTarefa from './TemporizadorTarefa.vue'
 
 export default defineComponent({
     name: 'FormularioTarefa',
     components: {
-        CronometroFormulario
-    },
-    data (){
-        return{
-            tempoEmSegundos: 0,
-            cronometro: 0
-        }
-    },
-    // computed
-    methods: {
-        iniciar() {
-            //Começar a contagem
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1
-            }, 1000)
-        },
-        finalizar() {
-            clearInterval(this.cronometro)
-        }
+        TemporizadorTarefa
     }
 })
 </script>
